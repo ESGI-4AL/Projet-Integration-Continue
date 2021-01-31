18b3e7fc509f9abcf884d703d8a0c970538e14ab
Ayoub.Zekhnine <ayoub.zekhnine@STDE.EFS.SANTE.BAN>
2021-01-31 15:11:38 +0100

    commit voyage App

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
-  res.send('changer !');
+  res.render('index', { country:process.env.COUNTRY })
 });
 
 var server = app.listen(3000, function () {
